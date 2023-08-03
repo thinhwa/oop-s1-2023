77147b10c52e4bcb955e0722abc7049b6003f95b
thinhwa <140475146+thinhwa@users.noreply.github.com>
2023-08-03 15:33:58 +0930

    done

---
+++ function-1-2.cpp
@@ -0,0 +1,10 @@
+int is_identity(int array[10][10]) {
+  for (int i = 0; i < 10; ++i){
+    for (int j=0; j <10; ++j){
+    if ((i == j && array[i][j] != 1) || (i != j && array[i][j] != 0 )) {
+      return 0;
+    }
+    }
+}
+return 1;
+}
