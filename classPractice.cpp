12f9724bc41eb233d7b69762b7d9cab066caf6d8
thinhwa <140475146+thinhwa@users.noreply.github.com>
2023-09-06 22:42:40 +0930

    finished

--- classPractice.cpp
+++ classPractice.cpp
@@ -7,9 +7,11 @@ class Honda {
 
 
 void drive(){
+    std::cout<< "\n"<< "\n";
     std::cout<< "Vrrm Vrmm"<< "\n" << "Now accelerating!" << std::endl;
 }
 void brake(){
+    std::cout<< "\n"<< "\n";
     std::cout<< "..."<< "\n"<< "Now braking!"<< std::endl;
 }
 };
@@ -18,9 +20,12 @@ int main(){
     Honda car1;
     car1.model = "Honda Civic";
     car1.year = 2005;
-
+    std::cout<< "\n"<< "\n";
+    std::cout << car1.model << std::endl;
+    std::cout << car1.year << std::endl;
     car1.drive();
     car1.brake();
     
+
     return 0;
 }
